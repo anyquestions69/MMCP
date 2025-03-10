4453d46392cf4331ac80ad8236aa6da6d286b241
anyquestions69 <juuzodes@yandex.ru>
2025-03-10 18:00:05 +0300

    не правильно работает пока

--- ПЗ1/main.py
+++ ПЗ1/main.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def add_big_numbers(a:List[int], b:List[int], B:int) -> List[int]:
+def add_big_numbers(a:List[int], b:List[int], B:int) -> str:
     n:int = len(a)
     c:int = 0  # Перенос
     w:List[int] = []  # Результат
@@ -14,16 +14,20 @@ def add_big_numbers(a:List[int], b:List[int], B:int) -> List[int]:
     if c > 0:
         w.append(c)  # Добавляем последний перенос
 
-    return w
+    result:str = ''.join(map(str, reversed(w)))
+    return result
 
 def array_from_str(s:str) -> List[int]:
+    for char in reversed(s):
+        assert char is int
+    
     return [int(char) for char in reversed(s)]
 
 
 # Пример использования
-a:List[int] = array_from_str("123456789")  # Число 456
-b:List[int] = array_from_str("987654321") # Число 789
-B:int = 16  # Основание системы счисления
+a:List[int] = array_from_str("832499128832")  # Число 456
+b:List[int] = array_from_str("2490854989") # Число 789
+B:int = 2  # Основание системы счисления
 print(a)
 result = add_big_numbers(a, b, B)
 print("Результат:", result)  # Вывод: [5, 4, 2, 1] (число 1245)
